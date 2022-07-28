09471e59bc901e8f58ad0b82b347031b7d00dd85
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-28 17:43:35 +0530

    See the comparision when adding the appbar alone and when adding with the toolbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 // Test -------------------------- Importing the Packages ---------------------------------
 import React from "react";
-import { AppBar } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 
 // Test -------------------------- Importing the styles / other components ----------------
 
 // Test -------------------------- The current component ----------------------------------
 const Header = () => {
-  return <AppBar>Header</AppBar>;
+  return (
+    <AppBar position="static">
+        Header
+      <Toolbar>Header</Toolbar>
+    </AppBar>
+  );
 };
 
 // Test -------------------------- Exporting the current component ------------------------
