6106bb5237c975818f4b3c8bcb5782acc74ece2e
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-28 19:15:14 +0530

    Finally able to add the links to the navbar items, using the <Link> R-R-D, wrap it around the button, otherwise, styles are fucked

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import HomePage from "./pages/HomePage/HomePage";
 import CoinPage from "./pages/CoinPage/CoinPage";
 
 import { Routes, Route } from "react-router-dom";
+import ErrorPage from "./pages/404Page/ErrorPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path="/coins/:coinName" element={<CoinPage></CoinPage>}></Route>
+      <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
     </Routes>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,14 @@
 // Test -------------------------- Importing the Packages ---------------------------------
 import React from "react";
-import { AppBar, Button, Toolbar, Typography, List, ListItem } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  List,
+  Box,
+  IconButton,
+} from "@mui/material";
 
 import { Link } from "react-router-dom";
 // Test -------------------------- Importing the styles / other components ----------------
@@ -10,15 +18,32 @@ const Header = () => {
   const navbarOptions = ["Test 1", "Test 2", "Test 3"];
 
   return (
-    <AppBar color="primary" position="static">
+    <AppBar sx={{ backgroundColor: "#353434" }} position="static">
       <Toolbar>
         <Typography variant="h5">Crypto Hunter</Typography>
-        <List color="red">
-            <ListItem>Test 1</ListItem>
-        </List>
-        <Button color="secondary" variant="contained">
+
+        <Box sx={{ display: "flex" }}>
+          {navbarOptions.map((option) => (
+            <Link to={`/${option}`} style={{ textDecoration: "none" }}>
+              <Button key={option} variant="text" sx={{ color: "yellow" }}>
+                {option}
+              </Button>
+            </Link>
+          ))}
+        </Box>
+
+        <Button
+          sx={{
+            color: "yellow",
+            borderColor: "yellow",
+            "&:hover": { borderColor: "yellow", backgroundColor: "#4e4e2e" },
+          }}
+          variant="outlined"
+        >
           Login
         </Button>
+
+        <IconButton></IconButton>
       </Toolbar>
     </AppBar>
   );
