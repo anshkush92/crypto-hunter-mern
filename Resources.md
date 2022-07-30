712331e8fad8dc24d394a35725e14dbbf9a99998
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-30 14:25:32 +0530

    finally able to change the value in the select by passing the select value

--- Resources.md
+++ Resources.md
@@ -1,12 +1,22 @@
 - [Project](https://www.youtube.com/watch?v=QA6oTpMZp84&list=PLKhlp2qtUcSa_rX7glmB7HyFsEOEQa0Uk)
+
 - [Redux Toolkit](https://redux-toolkit.js.org/introduction/getting-started)
+
   - [Quick Start](https://redux-toolkit.js.org/tutorials/quick-start)
+
 - [Material UI](https://mui.com/material-ui/react-autocomplete/)
+
   - [Box](https://mui.com/material-ui/react-box/)
   - [AppBar](https://mui.com/material-ui/react-app-bar/)
     - [AppBar with Options](https://mui.com/#responsive-app-bar-with-drawer)
   - [Menu](https://mui.com/material-ui/api/menu/)
   - [Button](https://mui.com/material-ui/react-button/#main-content)
+    - [IconButton](https://mui.com/material-ui/react-button/#icon-button)
+  - [Dark Theme](https://mui.com/material-ui/customization/dark-mode/#main-content)
+    - [Custom Palette](https://mui.com/material-ui/customization/dark-mode/#dark-mode-with-a-custom-palette)
+    - The components section of this is GEM [Theme Generator Very Useful](https://bareynol.github.io/mui-theme-creator/)
+  - [Select Drop Down](https://mui.com/material-ui/react-select/)
+
 - [React Router DOM](httphttps://mui.com/material-ui/react-box/s://reactrouter.com/docs/en/v6/getting-started/overview)
   - [Navigation](https://reactrouter.com/docs/en/v6/getting-started/overview#navigation)
 - [Chart JS](https://www.chartjs.org/docs/latest/)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,11 @@ import {
   Typography,
   Box,
   IconButton,
+  Select,
+  MenuItem,
 } from "@mui/material";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import { Link } from "react-router-dom";
 
@@ -25,57 +29,83 @@ const Header = () => {
   const isDarkMode = useSelector((state) => state.toggleTheme.isDarkMode);
   const dispatch = useDispatch();
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: isDarkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <AppBar sx={{ backgroundColor: "#353434" }} position="static">
-      <Toolbar>
-        <Link
-          to="/"
-          style={{
-            background: "inherit",
-            color: "inherit",
-            textDecoration: "none",
-          }}
-        >
-          <Typography variant="h5">Crypto Hunter</Typography>
-        </Link>
-
-        <Box sx={{ display: "flex" }}>
-          {navbarOptions.map((option) => (
-            <Link key={option} to={`/${option}`} style={{ textDecoration: "none" }}>
-              <Button variant="text" sx={{ color: "yellow" }}>
-                {option}
-              </Button>
-            </Link>
-          ))}
-        </Box>
-
-        <Link to="/login" style={{ textDecoration: "none" }}>
-          <Button
-            sx={{
-              color: "yellow",
-              borderColor: "yellow",
-              "&:hover": { borderColor: "yellow", backgroundColor: "#4e4e2e" },
+    <ThemeProvider theme={darkTheme}>
+      <AppBar sx={{ backgroundColor: "#353434" }} position="static">
+        <Toolbar>
+          <Link
+            to="/"
+            style={{
+              background: "inherit",
+              color: "inherit",
+              textDecoration: "none",
             }}
-            variant="outlined"
           >
-            Login
-          </Button>
-        </Link>
+            <Typography variant="h5">Crypto Hunter</Typography>
+          </Link>
 
-        <IconButton
-          onClick={() => {
-            dispatch(toggleTheme());
-          }}
-          sx={{color: "yellow"}}
-        >
-          {isDarkMode ? (
-            <DarkModeIcon></DarkModeIcon>
-          ) : (
-            <LightModeIcon></LightModeIcon>
-          )}
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+          <Box sx={{ display: "flex" }}>
+            {navbarOptions.map((option) => (
+              <Link
+                key={option}
+                to={`/${option}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Button variant="text" sx={{ color: "yellow" }}>
+                  {option}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            <Button
+              sx={{
+                color: "yellow",
+                borderColor: "yellow",
+                "&:hover": {
+                  borderColor: "yellow",
+                  backgroundColor: "#4e4e2e",
+                },
+              }}
+              variant="outlined"
+            >
+              Login
+            </Button>
+          </Link>
+
+          <Box>
+            <Select label="Currency" sx={{ color: "yellow" }} autoWidth>
+              <MenuItem value="USD">USD</MenuItem>
+              <MenuItem value="INR">INR</MenuItem>
+              <MenuItem value="EUR">EUR</MenuItem>
+              <MenuItem value="RUS">RUS</MenuItem>
+            </Select>
+          </Box>
+
+          <Box>
+            <IconButton
+              onClick={() => {
+                dispatch(toggleTheme());
+              }}
+              sx={{ color: "yellow" }}
+            >
+              {isDarkMode ? (
+                <DarkModeIcon></DarkModeIcon>
+              ) : (
+                <LightModeIcon></LightModeIcon>
+              )}
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
