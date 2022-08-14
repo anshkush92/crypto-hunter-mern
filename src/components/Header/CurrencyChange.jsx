72fddb4c0114f63b1ab9b01b7c6519620ba29e2b
anshkush92college <imt_2020013@iiitm.ac.in>
2022-08-14 16:41:29 +0530

    Updated the currency change for better understanding of the current currency

--- src/components/Header/CurrencyChange.jsx
+++ src/components/Header/CurrencyChange.jsx
@@ -8,18 +8,16 @@ import useCountryData from "../../hooks/countryData/useCountryData";
 // Test -------------------------- The current component ----------------------------------
 const CurrencyChange = () => {
   // For changing the state of app, when currency is changed
-  const [currency, setCurrency] = useState("USD");
-  
-  // Need to update the symbol, when the currency is changed
-  const [symbol, setSymbol] = useState("$");
+  const [currency, setCurrency] = useState("$ (USD)");
 
   // Using the data from the country Rest APIs
   const { countryData } = useCountryData();
 
   // For changing the currency as well as the symbol
   const currencyHandler = (event, newValue) => {
-    setSymbol(newValue.symbol);
-    setCurrency(newValue);
+    // Used for enhancing to tell which currency is being used right now
+    // console.log(newValue);
+    setCurrency(`${newValue.symbol} (${newValue.label})`);
   };
 
   return (
@@ -28,7 +26,6 @@ const CurrencyChange = () => {
       size="small"
       onChange={currencyHandler}
       isOptionEqualToValue={(option, value) => option.value === value.value}
-      defaultValue="USD"
       value={currency}
       id="country-data"
       options={countryData}
@@ -41,7 +38,7 @@ const CurrencyChange = () => {
           key={option.name}
         >
           <img loading="lazy" width="20" src={option.flag} alt={option.name} />
-          {option.label} ({option.symbol})
+          {option.symbol} ({option.label})
         </Box>
       )}
       renderInput={(params) => (
