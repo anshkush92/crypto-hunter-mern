6248e4fe3a52736e00782fda6c9b1681c183e888
anshkush92college <imt_2020013@iiitm.ac.in>
2023-03-26 00:03:51 +0530

    Host changes are reflected on the running container when using bind mounts, working fine bind mounts
    Signed-off-by: anshkush92college <imt_2020013@iiitm.ac.in>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import "./App.css";
-import { useEffect } from "react";
+import './App.css';
+import { useEffect } from 'react';
 
-import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "./features/userHandler/userHandler";
-import { setFavoriteCoinsList } from "./features/coinsList/coinsList";
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from './features/userHandler/userHandler';
+import { setFavoriteCoinsList } from './features/coinsList/coinsList';
 
-import HomePage from "./pages/HomePage/HomePage";
-import CoinPage from "./pages/CoinPage/CoinPage";
+import HomePage from './pages/HomePage/HomePage';
+import CoinPage from './pages/CoinPage/CoinPage';
 
-import { Routes, Route } from "react-router-dom";
-import ErrorPage from "./pages/404Page/ErrorPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SignupPage from "./pages/SignupPage/SignupPage";
-import FavoritePage from "./pages/FavoritePage/FavoritePage";
+import { Routes, Route } from 'react-router-dom';
+import ErrorPage from './pages/404Page/ErrorPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import FavoritePage from './pages/FavoritePage/FavoritePage';
 
-import { db } from "./firebase";
-import { doc, onSnapshot } from "firebase/firestore";
+import { db } from './firebase';
+import { doc, onSnapshot } from 'firebase/firestore';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -29,7 +29,7 @@ const App = () => {
 
   // Getting the user from the local storage
   useEffect(() => {
-    const input = JSON.parse(localStorage.getItem("user"));
+    const input = JSON.parse(localStorage.getItem('user'));
     // console.log(`Input`, input);
     if (input) {
       dispatch(loginUser(input));
@@ -40,14 +40,14 @@ const App = () => {
 
   useEffect(() => {
     if (user) {
-      const coinRef = doc(db, "favorite", user.uid);
+      const coinRef = doc(db, 'favorite', user.uid);
       const unsubscribe = onSnapshot(coinRef, (doc) => {
         if (doc.exists()) {
           // console.log("Document data:", doc.data());
           dispatch(setFavoriteCoinsList(doc.data().coins));
         } else {
           // doc.data() will be undefined in this case
-          console.log("items in the favorite list: 0");
+          console.log('items in the favorite list: 0');
         }
       });
       return () => {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 // Test -------------------------- Importing the Packages ---------------------------------
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 // Test -------------------------- Importing the styles / other components ----------------
-import AvatarMenu from "../AvatarMenu/AvatarMenu";
-import CurrencyChange from "./CurrencyChange";
-import AuthButton from "./AuthButton";
+import AvatarMenu from '../AvatarMenu/AvatarMenu';
+import CurrencyChange from './CurrencyChange';
+import AuthButton from './AuthButton';
 
 // Test -------------------------- The current component ----------------------------------
 const Header = () => {
@@ -15,11 +15,11 @@ const Header = () => {
   return (
     <>
       {/* The Navigation Bar */}
-      <AppBar sx={{ backgroundColor: "#393E46" }} position="static">
+      <AppBar sx={{ backgroundColor: '#393E46' }} position="static">
         <Toolbar
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
+            display: 'flex',
+            justifyContent: 'space-between',
             px: { xs: 1, sm: 2 },
             py: 1,
           }}
@@ -29,14 +29,14 @@ const Header = () => {
             <Link
               to="/"
               style={{
-                background: "inherit",
-                color: "inherit",
-                textDecoration: "none",
+                background: 'inherit',
+                color: 'inherit',
+                textDecoration: 'none',
               }}
             >
               <Typography
                 variant="h5"
-                sx={{ fontSize: { xs: "18px", sm: "24px" }, color: "#F7F7F7" }}
+                sx={{ fontSize: { xs: '18px', sm: '24px' }, color: '#F7F7F7' }}
               >
                 Crypto Hunter
               </Typography>
@@ -46,8 +46,8 @@ const Header = () => {
           {/* Theme Changer + Auth Buttons */}
           <Box
             sx={{
-              display: "flex",
-              alignItems: "center",
+              display: 'flex',
+              alignItems: 'center',
               gap: { xs: 0.5, sm: 1 },
             }}
           >
